6637cff5b4798ede23c9532312336cb1f8877b81
paul.bandi <paul.bandi@emanuel.ro>
2021-06-10 01:38:32 +0300

    Adding files

---
+++ AmazonPrimeAPI/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmazonPrimeAPI.Models
+{
+    public class Movie
+    {
+        public Guid id { get; internal set; }
+        public String name { get; set; }
+        public int duration { get; set; }
+    }
+}
---
+++ AmazonPrimeAPI/MovieController/MovieController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AmazonPrimeAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmazonPrimeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+
+        private static List<Movie> movies = new List<Movie>()
+        {
+            new Movie() { id = Guid.NewGuid(), name = "Harry Potter", duration = 2 },
+            new Movie() { id = Guid.NewGuid(), name = "Jumanji", duration = 1 },
+            new Movie() { id = Guid.NewGuid(), name = "Frozen", duration = 2 },
+            new Movie() { id = Guid.NewGuid(), name = "Tres metros sobre el cielo", duration = 3 },
+        };
+
+        [HttpGet]
+        public Movie[] Get()
+        {
+            return movies.ToArray();
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Movie movie)
+        {
+            if (movie.id == Guid.Empty)
+                movie.id = Guid.NewGuid();
+
+            movies.Add(movie);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Movie movie)
+        {
+            Movie currentMovie = movies.FirstOrDefault(m => m.id == movie.id);
+            currentMovie.name = movie.name;
+            currentMovie.duration = movie.duration;
+        }
+
+        [HttpDelete]
+        public void Delete(Guid id)
+        {
+            movies.RemoveAll(movie => movie.id == id);
+        }
+    }
+}
